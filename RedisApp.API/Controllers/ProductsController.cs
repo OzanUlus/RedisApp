a28184711fb6e4c2321d3f06f8ff792c830b2528
Ozan Ulus <ilkerozanulus@gmail.com>
2024-06-12 16:01:59 +0300

    created redis connection

---
+++ RedisApp.API/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RedisApp.API.Models;
+using RedisApp.API.Repository;
+using RedisApp.Cache;
+
+namespace RedisApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+   
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id) 
+        {
+          var data = await _productRepository.GetAsync(id);
+            return Ok(data);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllProduct() 
+        {
+          var datas = await _productRepository.GetAllAsync();
+            return Ok(datas);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(Product product) 
+        {
+            var data = await _productRepository.CreateAsync(product);
+            return Created(string.Empty,data);
+        }
+    }
+}
--- RedisApp.API/Program.cs
+++ RedisApp.API/Program.cs
@@ -1,40 +1,52 @@
 using Microsoft.EntityFrameworkCore;
 using RedisApp.API.Models;
 using RedisApp.API.Repository;
+using RedisApp.Cache;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext>(opt => {
-
-    opt.UseInMemoryDatabase(databaseName:"RedisApp");
-});
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-
-var app = builder.Build();
-
-using (var scope = app.Services.CreateScope()) 
+internal class Program
 {
-  var dbContex = app.Services.GetRequiredService<AppDbContext>();
-    dbContex.Database.EnsureCreated();
-}
-
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
+    private static void Main(string[] args)
     {
-        app.UseSwagger();
-        app.UseSwaggerUI();
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddDbContext<AppDbContext>(opt =>
+        {
+
+            opt.UseInMemoryDatabase(databaseName: "RedisApp");
+        });
+        builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+        builder.Services.AddSingleton<RedisService>(sp => {
+            return new RedisService(builder.Configuration["CacheOptions:Url"]);
+        });
+
+        var app = builder.Build();
+
+        using (var scope = app.Services.CreateScope())
+        {
+            var dbContex = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            dbContex.Database.EnsureCreated();
+        }
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+}
